35e6700785adcdf172d033e11503883b1bd6d984
agishevatatiana <tatiana.agisheva@hiqo-solutions.com>
2021-02-06 15:19:29 +0300

    separate breadcrumbs and userlist to components

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,15 +1,9 @@
 import React, {Component} from 'react';
-import Typography from '@material-ui/core/Typography';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@material-ui/core/Link';
-import Avatar from '@material-ui/core/Avatar';
-import GroupAddIcon from '@material-ui/icons/GroupAdd';
-import Tooltip from '@material-ui/core/Tooltip';
-import Fab from '@material-ui/core/Fab';
 
 import { Project, User } from '../models/models';
 import { getProjectByKey, getUsersByProjectKey } from '../mocks/mocks';
+import Breadcrumbs from '../components/Breadcrumbs';
+import UsersList from '../components/UsersList';
 
 type ProjectProps = {
     match: any;
@@ -47,37 +41,11 @@ class project extends Component<ProjectProps, ProjectState> {
 
     render() {
         const { project, users } = this.state;
-        const breadcrumbs = (
-            <Breadcrumbs aria-label="breadcrumb">
-                <Link color="inherit" component={RouterLink} to="/">
-                    Projects
-                </Link>
-                <Typography color="textPrimary">{project?.name} board</Typography>
-            </Breadcrumbs>
-        );
-
-        // parse users of project
-        const mapUsers = users.map((user) =>{
-            return user.avatar
-                ? <Fab key={user.key}><Avatar alt={user.full_name} src={user.avatar}></Avatar></Fab>
-                : <Fab key={user.key}><Avatar alt={user.full_name}>{user.full_name.substring(0, 1)}</Avatar></Fab>
-        });
-        const addUserBtn = (
-            <Fab key="addUserBtn" >
-                <Tooltip title="Add people (currently not available)">
-                    <Avatar>
-                        <GroupAddIcon />
-                    </Avatar>
-                </Tooltip>
-            </Fab>
-        );
-        mapUsers.push(addUserBtn);
-        // ---parse users of project
-
+        const breadcrumbsLinks = [{ to: '/', title: 'Projects' }];
         return (
             <div>
-                {breadcrumbs}
-                {mapUsers}
+                <Breadcrumbs links={breadcrumbsLinks} current={project?.name} />
+                <UsersList users={users} />
             </div>
         );
     }
